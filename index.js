7a84806734355016b707b01f4900a7e8a0b51bf2
Natasha Bartley <+>
2023-03-25 18:06:34 -0400

    completed lab

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+function receivesAFunction(spy) {
+    return spy();
+  }
+
+  function returnsANamedFunction() {
+    return function fn() {};
+  }
+  function returnsAnAnonymousFunction() {
+    return () => {};
+  }
